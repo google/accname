3c7babe56cce833371f879b533564de568339695
Oisín Nolan <oinolan@tcd.ie>
2020-06-29 19:04:55 +0100

    Implemented suggested changes.

--- src/accessible_name_test.ts
+++ src/accessible_name_test.ts
@@ -1,22 +1,19 @@
-import { getTextNodeAccessibleName } from '../src/accessible-name';
-import { JSDOM } from 'jsdom';
+import { getTextNodeAccessibleName } from './accessible_name';
 
 describe('The getTextNodeAccessibleName function', function() {
 
-  const dom: JSDOM = new JSDOM('<!DOCTYPE html><body></body>');
-  const document: Document = dom.window.document;
   beforeEach(function() {
     document.body.innerHTML = '';
   });
 
   it('returns text content if there is any', function() {
-    document.body.innerHTML = '<div id=\'foo\'>Hello there</div>';
+    document.body.innerHTML = '<div id="foo">Hello there</div>';
     const textNode: Node = document.getElementById('foo')!.childNodes[0];
     expect(getTextNodeAccessibleName(textNode)).toBe('Hello there');
   });
 
   it('returns empty string if there is no text content', function() {
-    document.body.innerHTML = '<div id=\'foo\'></div>';
+    document.body.innerHTML = '<div id="foo"></div>';
     const textNode: Node = document.getElementById('foo')!.childNodes[0];
     expect(getTextNodeAccessibleName(textNode)).toBe('');
   });
