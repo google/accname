09c4f35c0b62000d5ad7bcc6490dc29fcc07fa22
Oisín Nolan <oinolan@tcd.ie>
2020-07-31 10:59:48 +0100

    Refectored code for better readability and genericity

--- validation/node_ref.ts
+++ validation/node_ref.ts
@@ -1,12 +1,20 @@
 import {ElementHandle, CDPSession, Page} from 'puppeteer';
 import {Protocol} from 'devtools-protocol';
 
+// NodeRef interface provides multiple references to the same DOM node.
 export interface NodeRef {
+  // If the selector property is present then querySelector(selector) returns the referenced node.
   selector?: string;
   handle: ElementHandle;
   backendId: Protocol.DOM.BackendNodeId;
 }
 
+/**
+ * Create a nodeRef for the node uniquely identified by 'selector'
+ * @param selector - Selector uniquely identifying the node to be referenced.
+ * @param client - CDPSession for page.
+ * @param page - Page containing the node for whom a NodeRef will be created.
+ */
 export async function getNodeRefFromSelector(selector: string, client: CDPSession, page: Page): Promise<NodeRef | null> {
 
   const getDocumentResponse = await client.send('DOM.getDocument') as Protocol.DOM.GetDocumentResponse ;
@@ -30,8 +38,15 @@ export async function getNodeRefFromSelector(selector: string, client: CDPSessio
   return null;
 }
 
+/**
+ * Create a nodeRef using a DevTools Protocol BackendNodeId.
+ * @param backendId - BackendNodeId for the node whose nodeRef is being created.
+ * @param client - CDPSession for page.
+ * @param page - Page in which the node whose nodeRef is being created exists.
+ */
 export async function getNodeRefFromBackendId(backendId: Protocol.DOM.BackendNodeId, client: CDPSession, page: Page): Promise<NodeRef | null> {
 
+  // Use Protocol.Runtime.RemoteObject to reliably get Protocol.DOM.NodeId
   const resolveNodeResponse = await client.send('DOM.resolveNode', {
     backendNodeId: backendId
   }) as Protocol.DOM.ResolveNodeResponse;
@@ -40,17 +55,27 @@ export async function getNodeRefFromBackendId(backendId: Protocol.DOM.BackendNod
     objectId: resolveNodeResponse.object.objectId
   }) as Protocol.DOM.RequestNodeResponse;
 
+  const markerAttribute = randomString();
+
+  // Mark the node using a unique attribute
   await client.send('DOM.setAttributeValue', {
     nodeId: requestNodeResponse.nodeId,
-    name: 'nodeRefMarker',
+    name: markerAttribute,
     value: 'true'
   });
 
-  const nodeHandle = await page.$('[nodeRefMarker]');
+  // Find the referenced node using that unique attribute, allowing us
+  // to get an ElementHandle for that node.
+  const nodeHandle = await page.$('[' + markerAttribute + ']');
   if (nodeHandle) {
-    await page.evaluate(elem => elem.removeAttribute('nodeRefMarker'), nodeHandle);
+    // Remove the marker attribute.
+    await page.evaluate((elem, markerAttribute) => elem.removeAttribute(markerAttribute), nodeHandle, markerAttribute);
     return {backendId: backendId, handle: nodeHandle};
   }
 
   return null;
+}
+
+const randomString = () => {
+  return 'noderefMarker-' + Math.random().toString(36).substr(2, 5);
 }
--- validation/validate.ts
+++ validation/validate.ts
@@ -9,7 +9,7 @@ import {NodeRef, getNodeRefFromSelector, getNodeRefFromBackendId} from './node_r
 import {ourLibSource} from './our_lib_source';
 import * as axe from 'axe-core';
 
-// TODO: Take user input rather than hard coded
+// TODO: Take user input from UI rather than hard coded
 const INPUT_HTML = `
   <html>
     <head>
@@ -19,7 +19,12 @@ const INPUT_HTML = `
     </head>
     <body>
       <button id="bar">Hello<div id="baz">world</div></button>
-      <div accnameComparisonTarget aria-labelledby="bar baz"></div>
+      <div aria-labelledby="bar baz"></div>
+      <label for="woop">world</label>
+      <label>
+        Hello
+        <input accnameComparisonTarget id="woop"/>
+      </label>
     </body>
   </html>
 `;
@@ -30,22 +35,28 @@ const INPUT_HTML = `
   });
   const page = await browser.newPage();
   await page.goto('data:text/html,' + INPUT_HTML);
-
   const client = await page.target().createCDPSession();
 
   await loadAccNameLibraries(page);
   const targetNodeRef = await getNodeRefFromSelector('[accnameComparisonTarget]', client, page);
   if (targetNodeRef) {
     const accnames = await runComparison(targetNodeRef, page);
-    console.log(accnames);
+    console.log('AccName comparison:\n', accnames);
 
     const htmlUsedByChrome = await getHTMLUsedByChrome(targetNodeRef, client, page);
-    console.log(htmlUsedByChrome);
+    console.log('\nHTML used by Chrome:\n', htmlUsedByChrome);
   }
 
   await browser.close();
 })();
 
+/**
+ * Run a series of AccName implementations on the same node.
+ * @param nodeRef - NodeRef representing the target node for comparison.
+ * @param page - Page in which to run the implementations.
+ * @return - A mapping from implementation name to the accessible name produced
+ * by that implementation.
+ */
 async function runComparison(nodeRef: NodeRef, page: Page): Promise<{[key: string]: string}> {
   const accnames: {[key: string]: string} = {};
 
@@ -73,6 +84,10 @@ async function runComparison(nodeRef: NodeRef, page: Page): Promise<{[key: strin
   return accnames;
 }
 
+/**
+ * Prepare AccName implementation libraries to be run on the current page.
+ * @param page - The page in which the libraries
+ */
 async function loadAccNameLibraries(page: Page) {
   // Load axe-core
   await page.evaluate(axe.source);
@@ -87,7 +102,7 @@ async function loadAccNameLibraries(page: Page) {
     }
   `);
 
-  // Load Bryan Garaventa's Prototype
+  // Load Bryan Garaventa's Prototype from github repo
   const bgPrototypeSource = await (async () => {
     const response = await fetch('https://whatsock.github.io/w3c-alternative-text-computation/Sample%20JavaScript%20Recursion%20Algorithm/recursion.js');
     const responseBody = await response.text();
@@ -99,11 +114,14 @@ async function loadAccNameLibraries(page: Page) {
   await page.evaluate(ourLibSource);
 }
 
-async function getHTMLUsedByChrome(
-  nodeRef: NodeRef,
-  client: CDPSession,
-  page: Page
-): Promise<string> {
+/**
+ * Get a string containing the HTML used by Chrome DevTools to compute the accessible name
+ * for nodeRef.
+ * @param nodeRef - The node whose accessible name is being computed.
+ * @param client - The CDPSession for page.
+ * @param page - The page in which to run the accessible name computation.
+ */
+async function getHTMLUsedByChrome(nodeRef: NodeRef, client: CDPSession, page: Page): Promise<string> {
   const nodesUsedByChrome = await getNodesUsedByChrome(nodeRef, client, page);
   const relevantNodes = await removeRedundantNodes(nodesUsedByChrome, page);
   let htmlString = '';
@@ -113,11 +131,20 @@ async function getHTMLUsedByChrome(
   return htmlString;
 }
 
+/**
+ * Remove any nodes in nodeRefs that have an ancestor in nodeRefs.
+ * @param nodeRefs - Array of nodes from which redundant nodes are being removed
+ * @param page - Page containing the nodes in nodeRefs
+ */
 async function removeRedundantNodes(nodeRefs: NodeRef[], page: Page): Promise<NodeRef[]> {
   const redundantNodes: NodeRef[] = [];
   for (const nodeRefA of nodeRefs) {
     for (const nodeRefB of nodeRefs) {
+      // Any node that has an ancestor in nodeRefs is considered redundant because
+      // outerHTML includes all descendants.
       const isRedundant = await page.evaluate((nodeA, nodeB) => nodeA.contains(nodeB), nodeRefA.handle, nodeRefB.handle);
+      // nodeA contains nodeA, so we must ensure that nodeRefA !== nodeRefB or all
+      // nodes will be considered redundant.
       if (isRedundant && nodeRefA !== nodeRefB) {
         redundantNodes.push(nodeRefB);
       }
@@ -126,26 +153,28 @@ async function removeRedundantNodes(nodeRefs: NodeRef[], page: Page): Promise<No
   return nodeRefs.filter(nodeRef => !redundantNodes.includes(nodeRef));
 }
 
-async function getNodesUsedByChrome(
-  nodeRef: NodeRef,
-  client: CDPSession,
-  page: Page
-): Promise<NodeRef[]> {
+/**
+ * Gets all nodes used by Chrome to compute the accessible name for nodeRef.
+ * @param nodeRef - Node whose accessible name is being computed.
+ * @param client - CDPSession for page.
+ * @param page - Page containing nodeRef.
+ */
+async function getNodesUsedByChrome(nodeRef: NodeRef, client: CDPSession, page: Page): Promise<NodeRef[]> {
   const stack: NodeRef[] = [];
-  const visitedNodes: NodeRef[] = [];
+  const nodesUsed: NodeRef[] = [];
+  // Track backendIds of visited nodes to avoid infinite cycle.
+  const visitedNodes: Protocol.DOM.BackendNodeId[] = [];
   stack.push(nodeRef);
   // Iterative DFS traverses nodes connected by label references
   while (stack.length > 0) {
     const currentNodeRef = stack.pop();
-    visitedNodes.push(currentNodeRef!);
+    nodesUsed.push(currentNodeRef!);
 
     const axTree = (await client.send('Accessibility.getPartialAXTree', {
-      backendNodeId: currentNodeRef?.backendId,
+      backendNodeId: currentNodeRef!.backendId,
     })) as Protocol.Accessibility.GetPartialAXTreeResponse;
 
-    // Nodes in axTree.nodes are in order of decreasing depth in the DOM.
-    // We traverse all nodes descendant of currentNodeId, checking whether
-    // each one contains a label reference.
+    // Check if any AXNodes descandant of currentNodeRef are labelled
     for (const axNode of axTree.nodes) {
       let labelNodes: Protocol.Accessibility.AXRelatedNode[] = [];
       const sources: Protocol.Accessibility.AXValueSource[] =
@@ -163,19 +192,21 @@ async function getNodesUsedByChrome(
         }
       }
 
+      // Repeat the process for all unvisited label nodes.
       for (const labelNode of labelNodes) {
         const labelNodeRef = await getNodeRefFromBackendId(labelNode.backendDOMNodeId, client, page);
-        if (labelNodeRef && !visitedNodes.includes(labelNodeRef)) {
+        if (labelNodeRef && !visitedNodes.includes(labelNodeRef.backendId)) {
           stack.push(labelNodeRef);
+          visitedNodes.push(labelNodeRef.backendId);
         }
       }
       // Stop iterating when we reach the current node :
       // we have checked all descendants.
-      if (axNode.backendDOMNodeId === currentNodeRef?.backendId) {
+      if (axNode.backendDOMNodeId === currentNodeRef!.backendId) {
         break;
       }
     }
   }
 
-  return visitedNodes;
+  return nodesUsed;
 }
