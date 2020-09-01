23dcc6fb602d5bc84b9e25d13ec2df369c66c4db
Oisín Nolan <oinolan@tcd.ie>
2020-09-01 16:53:34 +0100

    Added check for title if 2F returns empty string

--- src/lib/rule2F.ts
+++ src/lib/rule2F.ts
@@ -226,7 +226,9 @@ export function rule2F(
     .filter(textAlterantive => textAlterantive !== '')
     .join(' ');
 
+  const result = (cssBeforeContent + accumulatedText + cssAfterContent).trim();
+
   // #SPEC_ASSUMPTION (F.2) : that CSS generated content should be
   // concatenated to accumulatedText
-  return (cssBeforeContent + accumulatedText + cssAfterContent).trim();
+  return result === '' ? null : result;
 }
